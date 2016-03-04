ea673a6e5fcb1f8e55c6455d3ae20f304e5d6ef7
Shane Oston Stowe <shane@shaneoston.com>
2016-03-04 13:46:25 +0000

    editing van_spec.rb

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -12,7 +12,7 @@ describe Van do
     # end
     it 'can receive something (note: nil is something)' do
       allow(docking_station).to receive(:broken_bikes).and_return([1])
-      expect(subject.collect_broken_bikes(docking_station)).to be_a_kind_of(Array)
+      expect(subject).to respond_to(collect_broken_bikes(docking_station)).with(1).argument
     end
     # xit 'receives an array of broken bikes' do
     #   expect(subject.collect_broken_bikes(docking_station)).to
